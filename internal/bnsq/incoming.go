c976bd4f16a0afe02d6360f815fdad1d063b9eb2
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2019-08-25 16:37:12 -0700

    Add Incoming message pubsub

---
+++ internal/bnsq/incoming.go
@@ -0,0 +1,56 @@
+package bnsq
+
+import (
+	"context"
+
+	"github.com/jakebailey/irc"
+)
+
+const (
+	incomingTopic = "irc.incoming"
+)
+
+type Incoming struct {
+	Origin  string       `json:"origin"`
+	Message *irc.Message `json:"message"`
+}
+
+type IncomingPublisher struct {
+	publisher *publisher
+}
+
+func NewIncomingPublisher(addr string, opts ...PublisherOption) *IncomingPublisher {
+	return &IncomingPublisher{
+		publisher: newPublisher(addr, opts...),
+	}
+}
+
+func (p *IncomingPublisher) Run(ctx context.Context) error {
+	return p.publisher.run(ctx)
+}
+
+func (p *IncomingPublisher) Publish(origin string, m *irc.Message) error {
+	return p.publisher.publish(incomingTopic, &Incoming{
+		Origin:  origin,
+		Message: m,
+	})
+}
+
+type IncomingSubscriber struct {
+	Addr       string
+	Channel    string
+	Opts       []SubscriberOption
+	OnIncoming func(*Incoming)
+}
+
+func (s *IncomingSubscriber) Run(ctx context.Context) error {
+	subscriber := newSubscriber(s.Addr, incomingTopic, s.Channel, s.Opts...)
+
+	return subscriber.run(ctx, func(m *message) {
+		i := &Incoming{}
+		if err := m.payload(i); err != nil {
+			return
+		}
+		s.OnIncoming(i)
+	})
+}
---
+++ internal/bnsq/incoming_test.go
@@ -0,0 +1,75 @@
+package bnsq_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/fortytw2/leaktest"
+	"github.com/hortbot/hortbot/internal/bnsq"
+	"github.com/hortbot/hortbot/internal/pkg/ctxlog"
+	"github.com/hortbot/hortbot/internal/pkg/errgroupx"
+	"github.com/hortbot/hortbot/internal/pkg/ircx"
+	"github.com/hortbot/hortbot/internal/pkg/testutil"
+	"github.com/hortbot/hortbot/internal/pkg/testutil/nsqtest"
+	"gotest.tools/v3/assert"
+)
+
+func TestIncoming(t *testing.T) {
+	defer leaktest.Check(t)()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	logger := testutil.Logger(t)
+	ctx = ctxlog.WithLogger(ctx, logger)
+
+	addr, cleanup, err := nsqtest.New()
+	assert.NilError(t, err)
+	defer cleanup()
+
+	const channel = "blue"
+
+	received := make(chan *bnsq.Incoming, 10)
+
+	publisher := bnsq.NewIncomingPublisher(addr)
+
+	subscriber := bnsq.IncomingSubscriber{
+		Addr:    addr,
+		Channel: channel,
+		OnIncoming: func(n *bnsq.Incoming) {
+			received <- n
+		},
+	}
+
+	g := errgroupx.FromContext(ctx)
+
+	g.Go(publisher.Run)
+	g.Go(subscriber.Run)
+
+	m1 := ircx.PrivMsg("#foobar", "test")
+	m2 := ircx.PrivMsg("#someone", "other test")
+
+	assert.NilError(t, publisher.Publish("hortbot", m1))
+	assert.NilError(t, publisher.Publish("otherbot", m2))
+
+	got1 := <-received
+	got2 := <-received
+
+	got1.Message.Raw = ""
+	got2.Message.Raw = ""
+
+	g.Stop()
+
+	assert.DeepEqual(t, got1, &bnsq.Incoming{
+		Origin:  "hortbot",
+		Message: m1,
+	})
+
+	assert.DeepEqual(t, got2, &bnsq.Incoming{
+		Origin:  "otherbot",
+		Message: m2,
+	})
+
+	assert.Equal(t, len(received), 0)
+}
