7f1cc7610ef237647e341e443a65009a99d39648
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2019-10-16 20:36:30 -0700

    Refactor cbp tests as closures

--- internal/cbp/cbp_test.go
+++ internal/cbp/cbp_test.go
@@ -10,211 +10,151 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name  string
-		input string
-		nodes []cbp.Node
-	}{
-		{
-			name: "Empty string",
-		},
-		{
-			name:  "No actions",
-			input: `this is a test`,
-			nodes: []cbp.Node{cbp.TextNode(`this is a test`)},
-		},
-		{
-			name:  "Special characters",
-			input: `this is a (cool) test_1234`,
-			nodes: []cbp.Node{cbp.TextNode(`this is a (cool) test_1234`)},
-		},
-		{
-			name:  "Single action",
-			input: `this is a (_USER_) test`,
-			nodes: []cbp.Node{
-				cbp.TextNode(`this is a `),
-				cbp.ActionNode(
-					cbp.TextNode(`USER`),
-				),
-				cbp.TextNode(` test`),
-			},
-		},
-		{
-			name:  "Single action at start",
-			input: `(_USER_) test`,
-			nodes: []cbp.Node{
-				cbp.ActionNode(
-					cbp.TextNode(`USER`),
-				),
-				cbp.TextNode(` test`),
-			},
-		},
-		{
-			name:  "Single action at end",
-			input: `this is a (_USER_)`,
-			nodes: []cbp.Node{
-				cbp.TextNode(`this is a `),
-				cbp.ActionNode(
-					cbp.TextNode(`USER`),
-				),
-			},
-		},
-		{
-			name:  "Multiple actions",
-			input: `this is a (_USER_) test (_FOO BAR_) thing`,
-			nodes: []cbp.Node{
-				cbp.TextNode(`this is a `),
-				cbp.ActionNode(
-					cbp.TextNode(`USER`),
-				),
-				cbp.TextNode(` test `),
-				cbp.ActionNode(
-					cbp.TextNode(`FOO BAR`),
-				),
-				cbp.TextNode(` thing`),
-			},
-		},
-		{
-			name:  "Nested action",
-			input: `this is a (_USER (_NAME_) WOW_) test`,
-			nodes: []cbp.Node{
-				cbp.TextNode(`this is a `),
-				cbp.ActionNode(
-					cbp.TextNode(`USER `),
-					cbp.ActionNode(
-						cbp.TextNode(`NAME`),
-					),
-					cbp.TextNode(` WOW`),
-				),
-				cbp.TextNode(` test`),
-			},
-		},
-		{
-			name:  "RIP counter",
-			input: `RIP count reset to (_VARS_RIP-(_GAME_CLEAN_)_SET_0_), neat.`,
-			nodes: []cbp.Node{
-				cbp.TextNode(`RIP count reset to `),
-				cbp.ActionNode(
-					cbp.TextNode(`VARS_RIP-`),
-					cbp.ActionNode(
-						cbp.TextNode(`GAME_CLEAN`),
-					),
-					cbp.TextNode(`_SET_0`),
-				),
-				cbp.TextNode(`, neat.`),
-			},
-		},
-		{
-			name:  "Open paren at end with action",
-			input: `how (_ok_) cool(`,
-			nodes: []cbp.Node{
-				cbp.TextNode(`how `),
-				cbp.ActionNode(
-					cbp.TextNode("ok"),
-				),
-				cbp.TextNode(` cool(`),
-			},
-		},
-		{
-			name:  "Underscore at end with action",
-			input: `how (_ok_) cool_`,
-			nodes: []cbp.Node{
-				cbp.TextNode(`how `),
-				cbp.ActionNode(
-					cbp.TextNode("ok"),
-				),
-				cbp.TextNode(` cool_`),
-			},
-		},
-		{
-			name:  "_ next to (_",
-			input: `(_HEY_(_TESTING_ERROR_)_)`,
-			nodes: []cbp.Node{
-				cbp.ActionNode(
-					cbp.TextNode(`HEY_`),
-					cbp.ActionNode(
-						cbp.TextNode("TESTING_ERROR"),
-					),
-				),
-			},
-		},
-		{
-			name:  "_ next to _)",
-			input: `(_(_TESTING_ERROR_)_HEY_)`,
-			nodes: []cbp.Node{
-				cbp.ActionNode(
-					cbp.ActionNode(
-						cbp.TextNode("TESTING_ERROR"),
-					),
-					cbp.TextNode(`_HEY`),
-				),
-			},
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-
-			c, err := cbp.Parse(test.input)
+	runTest := func(name string, input string, result ...cbp.Node) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			c, err := cbp.Parse(input)
 			assert.Check(t, err)
-			assert.Check(t, cmp.DeepEqual(test.nodes, c, cmpopts.EquateEmpty()))
+			assert.Check(t, cmp.DeepEqual(result, c, cmpopts.EquateEmpty()))
 		})
 	}
+
+	runTest("Empty string", "")
+	runTest("No actions", "this is a test", cbp.TextNode(`this is a test`))
+	runTest("Special characters", "this is a (cool) test_1234", cbp.TextNode(`this is a (cool) test_1234`))
+
+	runTest("Single action", "this is a (_USER_) test",
+		cbp.TextNode(`this is a `),
+		cbp.ActionNode(
+			cbp.TextNode(`USER`),
+		),
+		cbp.TextNode(` test`),
+	)
+
+	runTest("Single action at start", "(_USER_) test",
+		cbp.ActionNode(
+			cbp.TextNode(`USER`),
+		),
+		cbp.TextNode(` test`),
+	)
+
+	runTest("Single action at end", "this is a (_USER_)",
+		cbp.TextNode(`this is a `),
+		cbp.ActionNode(
+			cbp.TextNode(`USER`),
+		),
+	)
+
+	runTest("Multiple actions", "this is a (_USER_) test (_FOO BAR_) thing",
+		cbp.TextNode(`this is a `),
+		cbp.ActionNode(
+			cbp.TextNode(`USER`),
+		),
+		cbp.TextNode(` test `),
+		cbp.ActionNode(
+			cbp.TextNode(`FOO BAR`),
+		),
+		cbp.TextNode(` thing`),
+	)
+
+	runTest("Nested action", "this is a (_USER (_NAME_) WOW_) test",
+		cbp.TextNode(`this is a `),
+		cbp.ActionNode(
+			cbp.TextNode(`USER `),
+			cbp.ActionNode(
+				cbp.TextNode(`NAME`),
+			),
+			cbp.TextNode(` WOW`),
+		),
+		cbp.TextNode(` test`),
+	)
+
+	runTest("RIP counter", "RIP count reset to (_VARS_RIP-(_GAME_CLEAN_)_SET_0_), neat.",
+		cbp.TextNode(`RIP count reset to `),
+		cbp.ActionNode(
+			cbp.TextNode(`VARS_RIP-`),
+			cbp.ActionNode(
+				cbp.TextNode(`GAME_CLEAN`),
+			),
+			cbp.TextNode(`_SET_0`),
+		),
+		cbp.TextNode(`, neat.`),
+	)
+
+	runTest("Open paren at end with action", "how (_ok_) cool(",
+		cbp.TextNode(`how `),
+		cbp.ActionNode(
+			cbp.TextNode("ok"),
+		),
+		cbp.TextNode(` cool(`),
+	)
+
+	runTest("Underscore at end with action", "how (_ok_) cool_",
+		cbp.TextNode(`how `),
+		cbp.ActionNode(
+			cbp.TextNode("ok"),
+		),
+		cbp.TextNode(` cool_`),
+	)
+
+	runTest("Underscore next to open", "(_HEY_(_TESTING_ERROR_)_)",
+		cbp.ActionNode(
+			cbp.TextNode(`HEY_`),
+			cbp.ActionNode(
+				cbp.TextNode("TESTING_ERROR"),
+			),
+		),
+	)
+
+	runTest("Underscore next to close", "(_(_TESTING_ERROR_)_HEY_)",
+		cbp.ActionNode(
+			cbp.ActionNode(
+				cbp.TextNode("TESTING_ERROR"),
+			),
+			cbp.TextNode(`_HEY`),
+		),
+	)
 }
 
 func TestParseError(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name   string
-		input  string
-		err    cbp.Error
-		errMsg string
-	}{
-		{
-			name:  "Unclosed action",
-			input: "this action (_ is not closed",
-			err: cbp.Error{
-				Pos:  28,
-				Code: cbp.ErrorMissingClose,
-			},
-			errMsg: "syntax error at position 28; input ended unexpectedly",
-		},
-		{
-			name:  "Unexpected close",
-			input: "this action has a _) inside",
-			err: cbp.Error{
-				Pos:  18,
-				Code: cbp.ErrorUnexpectedClose,
-			},
-			errMsg: "syntax error at position 18; unexpected action close",
-		},
-		{
-			name:  "Bad nesting",
-			input: "foo (_ bar (_ baz _) fob",
-			err: cbp.Error{
-				Pos:  24,
-				Code: cbp.ErrorMissingClose,
-			},
-			errMsg: "syntax error at position 24; input ended unexpectedly",
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
-
-			c, err := cbp.Parse(test.input)
-			assert.Check(t, cmp.Nil(c))
-			assert.Check(t, cmp.Equal(test.err, err))
-			assert.Check(t, cmp.Error(err, test.errMsg))
+	runTest := func(name string, input string, want cbp.Error, errMsg string) {
+		t.Helper()
+		t.Run(name, func(t *testing.T) {
+			_, err := cbp.Parse(input)
+			assert.Check(t, cmp.Equal(want, err))
+			assert.Check(t, cmp.Error(err, errMsg))
 		})
 	}
+
+	runTest(
+		"Unclosed action",
+		"this action (_ is not closed",
+		cbp.Error{
+			Pos:  28,
+			Code: cbp.ErrorMissingClose,
+		},
+		"syntax error at position 28; input ended unexpectedly",
+	)
+
+	runTest(
+		"Unexpected close",
+		"this action has a _) inside",
+		cbp.Error{
+			Pos:  18,
+			Code: cbp.ErrorUnexpectedClose,
+		},
+		"syntax error at position 18; unexpected action close",
+	)
+
+	runTest(
+		"Bad nesting",
+		"foo (_ bar (_ baz _) fob",
+		cbp.Error{
+			Pos:  24,
+			Code: cbp.ErrorMissingClose,
+		},
+		"syntax error at position 24; input ended unexpectedly",
+	)
 }
 
 func TestBadErrorCode(t *testing.T) {
@@ -229,34 +169,16 @@ func TestBadErrorCode(t *testing.T) {
 }
 
 func BenchmarkParse(b *testing.B) {
-	tests := []struct {
-		name  string
-		input string
-	}{
-		{
-			name:  "No actions",
-			input: `This is just a cool test.`,
-		},
-		{
-			name:  "Parens",
-			input: `This is just a cool test. (Or is it?)`,
-		},
-		{
-			name:  "Single action",
-			input: `Hello, (_USER_)!`,
-		},
-		{
-			name:  "Nested",
-			input: `RIP count reset to (_VARS_RIP-(_GAME_CLEAN_)_SET_0_), neat.`,
-		},
-	}
-
-	for _, test := range tests {
-		test := test
-		b.Run(test.name, func(b *testing.B) {
+	runBenchmark := func(name string, input string) {
+		b.Run(name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				_, _ = cbp.Parse(test.input)
+				_, _ = cbp.Parse(input)
 			}
 		})
 	}
+
+	runBenchmark("No actions", "This is just a cool test.")
+	runBenchmark("Parens", "This is just a cool test. (Or is it?)")
+	runBenchmark("Single action", "Hello, (_USER_)!")
+	runBenchmark("Nested", "RIP count reset to (_VARS_RIP-(_GAME_CLEAN_)_SET_0_), neat.")
 }
