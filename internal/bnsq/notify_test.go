4d6695f4a2c859aacfca5867ac5167e765a7dad9
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2019-12-18 22:38:10 -0800

    Test correlation and spans in NSQ metadata

--- internal/bnsq/notify_test.go
+++ internal/bnsq/notify_test.go
@@ -7,10 +7,13 @@ import (
 
 	"github.com/fortytw2/leaktest"
 	"github.com/hortbot/hortbot/internal/bnsq"
+	"github.com/hortbot/hortbot/internal/pkg/correlation"
 	"github.com/hortbot/hortbot/internal/pkg/ctxlog"
 	"github.com/hortbot/hortbot/internal/pkg/docker/dnsq"
 	"github.com/hortbot/hortbot/internal/pkg/errgroupx"
 	"github.com/hortbot/hortbot/internal/pkg/testutil"
+	"github.com/rs/xid"
+	"go.opencensus.io/trace"
 	"gotest.tools/v3/assert"
 )
 
@@ -33,6 +36,8 @@ func TestNotify(t *testing.T) {
 	)
 
 	received := make(chan *bnsq.ChannelUpdatesNotification, 10)
+	cids := make(chan xid.ID, 10)
+	spans := make(chan trace.SpanContext)
 
 	publisher := bnsq.NewNotifyPublisher(addr)
 
@@ -40,8 +45,10 @@ func TestNotify(t *testing.T) {
 		Addr:    addr,
 		BotName: botName,
 		Channel: channel,
-		OnNotifyChannelUpdates: func(n *bnsq.ChannelUpdatesNotification, _ *bnsq.Metadata) error {
+		OnNotifyChannelUpdates: func(n *bnsq.ChannelUpdatesNotification, m *bnsq.Metadata) error {
 			received <- n
+			cids <- correlation.FromContext(m.Correlate(context.Background()))
+			spans <- m.ParentSpan()
 			return nil
 		},
 	}
@@ -51,16 +58,24 @@ func TestNotify(t *testing.T) {
 	g.Go(publisher.Run)
 	g.Go(subscriber.Run)
 
-	assert.NilError(t, publisher.NotifyChannelUpdates(ctx, botName))
+	id := xid.New()
+	ctx, span := trace.StartSpan(ctx, "TwitchToken")
+
+	assert.NilError(t, publisher.NotifyChannelUpdates(correlation.WithID(ctx, id), botName))
 	assert.NilError(t, publisher.NotifyChannelUpdates(ctx, "wrong"))
 
 	got := <-received
+	gotID := <-cids
+	gotSpan := <-spans
 
 	g.Stop()
+
+	assert.Equal(t, len(received), 0)
 
 	assert.DeepEqual(t, got, &bnsq.ChannelUpdatesNotification{
 		BotName: botName,
 	})
 
-	assert.Equal(t, len(received), 0)
+	assert.Equal(t, gotID, id)
+	assert.Equal(t, gotSpan.TraceID, span.SpanContext().TraceID)
 }
