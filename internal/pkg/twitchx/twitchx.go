120d28b5c35f9018c7e448b96aee8bf56970c588
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2020-01-06 22:22:49 -0800

    Fix and test FindBotToken

--- internal/pkg/twitchx/twitchx.go
+++ internal/pkg/twitchx/twitchx.go
@@ -16,35 +16,35 @@ import (
 // FindBotToken looks for the bot's token in the database, and fetches a token
 // if one has not yet been found.
 func FindBotToken(ctx context.Context, db boil.ContextExecutor, tw twitch.API, botName string) (*oauth2.Token, error) {
-	_, tok, err := findBotToken(ctx, db, tw, botName)
-	return tok, err
-}
-
-func findBotToken(ctx context.Context, db boil.ContextExecutor, tw twitch.API, botName string) (old, new *oauth2.Token, err error) {
 	botNameNull := null.StringFrom(botName)
 
 	token, err := models.TwitchTokens(models.TwitchTokenWhere.BotName.EQ(botNameNull)).One(ctx, db)
 	switch err {
 	case nil:
 	case sql.ErrNoRows:
-		return nil, nil, nil
+		return nil, nil
 	default:
-		return nil, nil, err
+		return nil, err
 	}
 
 	tok := modelsx.ModelToToken(token)
-	user, newTok, err := tw.GetUserForToken(ctx, tok)
+	_, newTok, err := tw.GetUserForToken(ctx, tok)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	if newTok == nil {
-		return tok, tok, nil
+		return tok, nil
 	}
 
-	if err := modelsx.UpsertToken(ctx, db, modelsx.TokenToModel(user.ID, tok)); err != nil {
-		return nil, nil, err
+	token.AccessToken = newTok.AccessToken
+	token.TokenType = newTok.TokenType
+	token.RefreshToken = newTok.RefreshToken
+	token.Expiry = newTok.Expiry
+
+	if err := modelsx.FullUpsertToken(ctx, db, token); err != nil {
+		return nil, err
 	}
 
-	return tok, newTok, nil
+	return newTok, nil
 }
---
+++ internal/pkg/twitchx/twitchx_test.go
@@ -0,0 +1,218 @@
+package twitchx_test
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"testing"
+	"time"
+
+	gocmp "github.com/google/go-cmp/cmp"
+	"github.com/hortbot/hortbot/internal/db/models"
+	"github.com/hortbot/hortbot/internal/db/modelsx"
+	"github.com/hortbot/hortbot/internal/pkg/apis/twitch"
+	"github.com/hortbot/hortbot/internal/pkg/apis/twitch/twitchfakes"
+	"github.com/hortbot/hortbot/internal/pkg/ctxlog"
+	"github.com/hortbot/hortbot/internal/pkg/oauth2x"
+	"github.com/hortbot/hortbot/internal/pkg/testutil"
+	"github.com/hortbot/hortbot/internal/pkg/twitchx"
+	"github.com/volatiletech/null"
+	"github.com/volatiletech/sqlboiler/boil"
+	"golang.org/x/oauth2"
+	"gotest.tools/v3/assert"
+	"gotest.tools/v3/assert/cmp"
+)
+
+var (
+	errTest  = errors.New("testing error")
+	tokenCmp = gocmp.Comparer(func(x, y oauth2.Token) bool {
+		return oauth2x.Equals(&x, &y)
+	})
+)
+
+func TestFindBotTokenNoBot(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := testContext(t)
+	defer cancel()
+
+	tw := &twitchfakes.FakeAPI{}
+	db := pool.FreshDB(t)
+	defer db.Close()
+
+	tok, err := twitchx.FindBotToken(ctx, db, tw, "hortbot")
+	assert.NilError(t, err)
+	assert.Assert(t, tok == nil)
+}
+
+func TestFindBotToken(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := testContext(t)
+	defer cancel()
+
+	tw := &twitchfakes.FakeAPI{}
+	db := pool.FreshDB(t)
+	defer db.Close()
+
+	orig := &models.TwitchToken{
+		TwitchID:     1234,
+		BotName:      null.StringFrom("hortbot"),
+		AccessToken:  "at-qwertyuiop1234567890",
+		TokenType:    "Bearer",
+		RefreshToken: "rt-qwertyuiop1234567890",
+		Expiry:       time.Now(),
+	}
+
+	origTok := modelsx.ModelToToken(orig)
+
+	assert.NilError(t, orig.Insert(ctx, db, boil.Infer()))
+
+	tok, err := twitchx.FindBotToken(ctx, db, tw, orig.BotName.String)
+	assert.NilError(t, err)
+
+	assert.Assert(t, cmp.DeepEqual(origTok, tok, tokenCmp))
+}
+
+func TestFindBotTokenGetError(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := testContext(t)
+	defer cancel()
+
+	tw := &twitchfakes.FakeAPI{
+		GetUserForTokenStub: func(context.Context, *oauth2.Token) (*twitch.User, *oauth2.Token, error) {
+			return nil, nil, errTest
+		},
+	}
+	db := pool.FreshDB(t)
+	defer db.Close()
+
+	orig := &models.TwitchToken{
+		TwitchID:     1234,
+		BotName:      null.StringFrom("hortbot"),
+		AccessToken:  "at-qwertyuiop1234567890",
+		TokenType:    "Bearer",
+		RefreshToken: "rt-qwertyuiop1234567890",
+		Expiry:       time.Now(),
+	}
+
+	assert.NilError(t, orig.Insert(ctx, db, boil.Infer()))
+
+	_, err := twitchx.FindBotToken(ctx, db, tw, "hortbot")
+	assert.Equal(t, err, errTest)
+}
+
+func TestFindBotTokenDBErr(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := testContext(t)
+	defer cancel()
+
+	tw := &twitchfakes.FakeAPI{}
+	db := pool.FreshDB(t)
+	defer db.Close()
+
+	breakDB(t, db)
+
+	_, err := twitchx.FindBotToken(ctx, db, tw, "hortbot")
+	assert.ErrorContains(t, err, "does not exist")
+}
+
+func TestFindBotTokenNew(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := testContext(t)
+	defer cancel()
+
+	newT := &models.TwitchToken{
+		TwitchID:     1234,
+		BotName:      null.StringFrom("hortbot"),
+		AccessToken:  "at-asdfghjkl0987654321",
+		TokenType:    "Bearer",
+		RefreshToken: "rt-asdfghjkl0987654321",
+		Expiry:       time.Now(),
+	}
+
+	newTok := modelsx.ModelToToken(newT)
+
+	tw := &twitchfakes.FakeAPI{
+		GetUserForTokenStub: func(context.Context, *oauth2.Token) (*twitch.User, *oauth2.Token, error) {
+			return &twitch.User{
+				ID: newT.ID,
+			}, newTok, nil
+		},
+	}
+	db := pool.FreshDB(t)
+	defer db.Close()
+
+	orig := &models.TwitchToken{
+		TwitchID:     1234,
+		BotName:      null.StringFrom("hortbot"),
+		AccessToken:  "at-qwertyuiop1234567890",
+		TokenType:    "Bearer",
+		RefreshToken: "rt-qwertyuiop1234567890",
+		Expiry:       time.Now(),
+	}
+
+	assert.NilError(t, orig.Insert(ctx, db, boil.Infer()))
+
+	tok, err := twitchx.FindBotToken(ctx, db, tw, "hortbot")
+	assert.NilError(t, err)
+	assert.Assert(t, cmp.DeepEqual(newTok, tok, tokenCmp))
+
+	upserted, err := models.TwitchTokens(models.TwitchTokenWhere.TwitchID.EQ(1234)).One(ctx, db)
+	assert.NilError(t, err)
+
+	upsertedTok := modelsx.ModelToToken(upserted)
+	assert.Assert(t, cmp.DeepEqual(newTok, upsertedTok, tokenCmp))
+}
+
+func TestFindBotTokenNewDBError(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := testContext(t)
+	defer cancel()
+
+	newT := &models.TwitchToken{
+		TwitchID:     1234,
+		BotName:      null.StringFrom("hortbot"),
+		AccessToken:  "at-asdfghjkl0987654321",
+		TokenType:    "Bearer",
+		RefreshToken: "rt-asdfghjkl0987654321",
+		Expiry:       time.Now(),
+	}
+
+	newTok := modelsx.ModelToToken(newT)
+
+	db := pool.FreshDB(t)
+	defer db.Close()
+	tw := &twitchfakes.FakeAPI{
+		GetUserForTokenStub: func(context.Context, *oauth2.Token) (*twitch.User, *oauth2.Token, error) {
+			breakDB(t, db)
+
+			return &twitch.User{
+				ID: newT.ID,
+			}, newTok, nil
+		},
+	}
+
+	orig := &models.TwitchToken{
+		TwitchID:     1234,
+		BotName:      null.StringFrom("hortbot"),
+		AccessToken:  "at-qwertyuiop1234567890",
+		TokenType:    "Bearer",
+		RefreshToken: "rt-qwertyuiop1234567890",
+		Expiry:       time.Now(),
+	}
+
+	assert.NilError(t, orig.Insert(ctx, db, boil.Infer()))
+
+	_, err := twitchx.FindBotToken(ctx, db, tw, "hortbot")
+	assert.ErrorContains(t, err, "does not exist")
+}
+
+func breakDB(t testing.TB, db *sql.DB) {
+	_, err := db.Exec("DROP TABLE twitch_tokens")
+	assert.NilError(t, err)
+}
+
+func testContext(t testing.TB) (context.Context, context.CancelFunc) {
+	t.Helper()
+	ctx := ctxlog.WithLogger(context.Background(), testutil.Logger(t))
+	return context.WithTimeout(ctx, 5*time.Minute)
+}
