168b2dc9f4a17f67997d107c2cd92c39d6312f3f
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2020-01-21 22:26:25 -0800

    Use canonical navbar burger code

--- internal/web/templates/base.qtpl.go
+++ internal/web/templates/base.qtpl.go
@@ -76,7 +76,7 @@ func StreamPageTemplate(qw422016 *qt422016.Writer, p Page) {
 	p.StreamPageBrand(qw422016)
 	qw422016.N().S(`</a>
 
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu" onclick="toggleBurger()">
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -107,10 +107,31 @@ func StreamPageTemplate(qw422016 *qt422016.Writer, p Page) {
 	qw422016.N().S(`
 
         <script>
-            function toggleBurger() {
-                document.querySelector(".burger").classList.toggle("is-active");
-                document.querySelector(".navbar-menu").classList.toggle("is-active");
+            document.addEventListener('DOMContentLoaded', () => {
+
+            // Get all "navbar-burger" elements
+            const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+            // Check if there are any navbar burgers
+            if ($navbarBurgers.length > 0) {
+
+            // Add a click event on each of them
+            $navbarBurgers.forEach( el => {
+                el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+                });
+            });
             }
+
+            });
         </script>
     </body>
 </html>
