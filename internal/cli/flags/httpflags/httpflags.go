e1f8d47b72fcd56668f60aa9cef09d819210e8ff
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2020-03-17 23:02:17 -0700

    Tweak untrusted HTTP client

--- internal/cli/flags/httpflags/httpflags.go
+++ internal/cli/flags/httpflags/httpflags.go
@@ -32,7 +32,8 @@ var Default = HTTP{
 // Client returns a new HTTP client configured based on the flags.
 func (h *HTTP) Client() *http.Client {
 	return &http.Client{
-		Timeout: h.Timeout,
+		Timeout:   h.Timeout,
+		Transport: filtertransport.DefaultTransport,
 	}
 }
 
@@ -49,20 +50,27 @@ func (h *HTTP) UntrustedClient(ctx context.Context) *http.Client {
 			auth = &proxy.Auth{User: h.UntrustedProxyUser, Password: h.UntrustedProxyPassword}
 		}
 
+		// TODO: safeDialer performs name resolution locally rather than the proxy. Is this safe?
 		dialer, err := proxy.SOCKS5("tcp", h.UntrustedProxy, auth, safeDialer)
 		if err != nil {
 			ctxlog.Fatal(ctx, "error creating SOCKS5 proxy dialer", zap.Error(err))
 		}
 
-		cli.Transport = &http.Transport{
-			Dial: dialer.Dial,
+		transport := filtertransport.DefaultTransport.Clone()
+
+		//lint:ignore SA1019 As a backup in case DialContext is nil.
+		transport.Dial = dialer.Dial //nolint
+
+		if ctxDialer, ok := dialer.(proxy.ContextDialer); ok {
+			transport.DialContext = ctxDialer.DialContext
+		} else {
+			transport.DialContext = nil
 		}
+
+		cli.Transport = transport
 	} else {
 		ctxlog.Warn(ctx, "no proxy provided for untrusted HTTP client")
-		cli.Transport = &http.Transport{
-			Dial:        safeDialer.Dial,
-			DialContext: safeDialer.DialContext,
-		}
+		cli.Transport = filtertransport.DefaultTransport
 	}
 
 	return cli
@@ -70,17 +78,17 @@ func (h *HTTP) UntrustedClient(ctx context.Context) *http.Client {
 
 type filteredDialer struct{}
 
-var safeDialer filteredDialer
-
 var (
-	_ proxy.Dialer        = safeDialer
-	_ proxy.ContextDialer = safeDialer
+	netDialer  net.Dialer
+	safeDialer *filteredDialer
+	_          proxy.Dialer        = safeDialer
+	_          proxy.ContextDialer = safeDialer
 )
 
-func (filteredDialer) Dial(network, addr string) (c net.Conn, err error) {
-	return filtertransport.FilterDial(context.Background(), network, addr, filtertransport.DefaultFilter, proxy.Direct.DialContext)
+func (*filteredDialer) Dial(network, addr string) (c net.Conn, err error) {
+	return filtertransport.FilterDial(context.Background(), network, addr, filtertransport.DefaultFilter, netDialer.DialContext)
 }
 
-func (filteredDialer) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
-	return filtertransport.FilterDial(ctx, network, addr, filtertransport.DefaultFilter, proxy.Direct.DialContext)
+func (*filteredDialer) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	return filtertransport.FilterDial(ctx, network, addr, filtertransport.DefaultFilter, netDialer.DialContext)
 }
