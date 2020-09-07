e6d865f505a38886ae7a5f9380a41d492a75a4e0
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2020-09-07 16:53:46 -0700

    Add missing redis trace

--- internal/db/redis/stats.go
+++ internal/db/redis/stats.go
@@ -1,15 +1,23 @@
 package redis
 
-import "context"
+import (
+	"context"
+
+	"go.opencensus.io/trace"
+)
 
 const builtinUsageStatsHash = "stats_builtin_usage"
 
 // IncrementBuiltinUsageStat increments a usage statistic by one.
 func (db *DB) IncrementBuiltinUsageStat(ctx context.Context, name string) error {
+	ctx, span := trace.StartSpan(ctx, "IncrementBuiltinUsageStat")
+	defer span.End()
 	return db.client.HIncrBy(ctx, builtinUsageStatsHash, name, 1).Err()
 }
 
 // GetBuiltinUsageStats gets all usage statistics.
 func (db *DB) GetBuiltinUsageStats(ctx context.Context) (map[string]string, error) {
+	ctx, span := trace.StartSpan(ctx, "GetBuiltinUsageStats")
+	defer span.End()
 	return db.client.HGetAll(ctx, builtinUsageStatsHash).Result()
 }
