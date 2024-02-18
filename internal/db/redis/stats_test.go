d4d719b0382bace1c7ae464c1e55c61e918f9592
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2024-02-17 22:35:11 -0800

    Fix mistake in redis stat test

--- internal/db/redis/stats_test.go
+++ internal/db/redis/stats_test.go
@@ -22,23 +22,23 @@ func TestBultinUsageStats(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	stats, err := db.GetActionUsageStats(ctx)
+	stats, err := db.GetBuiltinUsageStats(ctx)
 	assert.NilError(t, err)
 	assert.Equal(t, len(stats), 0)
 
-	err = db.IncrementActionUsageStat(ctx, "command")
+	err = db.IncrementBuiltinUsageStat(ctx, "command")
 	assert.NilError(t, err)
 
-	stats, err = db.GetActionUsageStats(ctx)
+	stats, err = db.GetBuiltinUsageStats(ctx)
 	assert.NilError(t, err)
 	assert.DeepEqual(t, stats, map[string]string{
 		"command": "1",
 	})
 
-	err = db.IncrementActionUsageStat(ctx, "command")
+	err = db.IncrementBuiltinUsageStat(ctx, "command")
 	assert.NilError(t, err)
 
-	stats, err = db.GetActionUsageStats(ctx)
+	stats, err = db.GetBuiltinUsageStats(ctx)
 	assert.NilError(t, err)
 	assert.DeepEqual(t, stats, map[string]string{
 		"command": "2",
