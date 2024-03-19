06c45e947fa04198b2b806545aba5a5b8c0fb63e
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2024-03-19 10:48:33 -0700

    fmt

--- internal/bot/handle.go
+++ internal/bot/handle.go
@@ -41,7 +41,6 @@ type Message interface {
 	Message() (message string, me bool)
 }
 
-
 // Handle handles a single IRC message, sent via the specific origin. It always
 // succeeds, but may log information about any internal errors.
 func (b *Bot) Handle(ctx context.Context, origin string, m Message) {
