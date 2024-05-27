df19f3bf904ddf6d04a8fd9086a70c7e8974f66a
zikaeroh <48577114+zikaeroh@users.noreply.github.com>
2024-05-26 20:21:26 -0700

    Generate useragent code, saving repo and binary size

--- internal/pkg/useragent/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd "${0%/*}"
-
-curl -L https://unpkg.com/user-agents/src/user-agents.json.gz > user-agents.json.gz
--- internal/pkg/useragent/useragent.go
+++ internal/pkg/useragent/useragent.go
@@ -3,76 +3,10 @@ package useragent
 // https://github.com/intoli/user-agents
 
 import (
-	"bytes"
-	"compress/gzip"
-	_ "embed" // For go:embed.
-
-	"github.com/hortbot/hortbot/internal/pkg/jsonx"
-	"github.com/hortbot/hortbot/internal/pkg/must"
 	"github.com/hortbot/hortbot/internal/version"
-	"github.com/mroth/weightedrand/v2"
 )
 
-//go:embed user-agents.json.gz
-var agents []byte
-
-type entry struct {
-	AppName    string `json:"appName"`
-	Connection struct {
-		Downlink      float64 `json:"downlink"`
-		EffectiveType string  `json:"effectiveType"`
-		RTT           int     `json:"rtt"`
-	} `json:"connection"`
-	Platform       string  `json:"platform"`
-	PluginsLength  int     `json:"pluginsLength"`
-	Vendor         string  `json:"vendor"`
-	UserAgent      string  `json:"userAgent"`
-	ViewportHeight int     `json:"viewportHeight"`
-	ViewportWidth  int     `json:"viewportWidth"`
-	DeviceCategory string  `json:"deviceCategory"`
-	ScreenHeight   int     `json:"screenHeight"`
-	ScreenWidth    int     `json:"screenWidth"`
-	Weight         float64 `json:"weight"`
-}
-
-const minThreshold = 100
-
-var chooser *weightedrand.Chooser[string, int]
-
-func init() {
-	r := must.Must(gzip.NewReader(bytes.NewReader(agents)))
-
-	var entries []*entry
-
-	must.NilError(jsonx.DecodeSingle(r, &entries))
-
-	minWeight := entries[0].Weight
-	for _, e := range entries {
-		if e.Weight < minWeight {
-			minWeight = e.Weight
-		}
-	}
-
-	factor := 1.0
-	for {
-		if minWeight*factor > minThreshold {
-			break
-		}
-		factor *= 10
-	}
-
-	choices := make([]weightedrand.Choice[string, int], 0, len(entries))
-	for _, e := range entries {
-		if e.DeviceCategory != "desktop" {
-			continue
-		}
-
-		choice := weightedrand.NewChoice(e.UserAgent, int(factor*e.Weight))
-		choices = append(choices, choice)
-	}
-
-	chooser = must.Must(weightedrand.NewChooser(choices...))
-}
+//go:generate go run generate.go
 
 var botUserAgent = "HortBot/" + version.Version()
 
